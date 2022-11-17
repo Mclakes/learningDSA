b788d5f4f4ef02ffe4e10eff75f88a3179972e0a
Mclakes <lekanhayo@gmail.com>
2022-11-17 12:46:37 +0100

    set and get in hash

--- main.js
+++ main.js
@@ -35,17 +35,67 @@
 
 // console.log("olalekan".length);
 
-let user = {
-    age: 54,
-    name: "kylie",
-    magic: true,
-    scream: function() {
-        console.log('ahhhhhh');
+// let user = {
+//     age: 54,
+//     name: "kylie",
+//     magic: true,
+//     scream: function() {
+//         console.log('ahhhhhh');
+//     }
+// }
+
+//  //0(1)
+// user.spell = 'aba ka da bra';
+// user
+
+// console.log(user.scream())
+
+// const a = new Map()
+// const b = new Set()
+
+class HashTable {
+    constructor(size) {
+        this.data = new Array(size);
+    }
+
+    _hash(key) {
+        let hash = 0;
+        for (let i= 0; i < key.length; i++) {
+            hash = (hash + key.charCodeAt(i) * i ) %
+            this.data.length
+            // console.log(hash)
+        }
+        return hash
+    }
+
+    set(key, value) {
+        let address = this._hash(key);
+        if (!this.data[address]) {
+            this.data[address] =[];
+        }
+        this.data[address].push([key, value])
+        return this.data
+    }
+    get(key) {
+        let address = this._hash(key);
+        const currentBucket = this.data[address];
+        if(currentBucket) {
+            for (let i = 0; i<currentBucket.length; i++){
+                if(currentBucket[i][0] === key ) {
+                    return currentBucket[i][1]
+                }
+            }
+        }
+        return undefined
     }
 }
 
- //0(1)
-user.spell = 'aba ka da bra';
-user
+const myHashTable = new HashTable(2)
+// myHashTable.set('grapes', 10000)
+// myHashTable.get('grapes')
+myHashTable.set('grapes', 10000)
+myHashTable.set('apples', 54)
 
-console.log(user.scream())
+// console.log(myHashTable._hash('grapes'))
+// console.log(myHashTable.set('grapes', 10000), myHashTable.set('apples', 54)) 
+console.log(myHashTable.get('grapes'));
