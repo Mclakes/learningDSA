5dbeb27b9e6129f48e2e38e24d20bd053cd62726
Mclakes <lekanhayo@gmail.com>
2022-11-17 23:28:06 +0100

    findingRecurringNumber

--- main.js
+++ main.js
@@ -53,64 +53,93 @@
 // const a = new Map()
 // const b = new Set()
 
-class HashTable {
-    constructor(size) {
-        this.data = new Array(size);
-    }
+// class HashTable {
+//     constructor(size) {
+//         this.data = new Array(size);
+//     }
 
-    _hash(key) {
-        let hash = 0;
-        for (let i= 0; i < key.length; i++) {
-            hash = (hash + key.charCodeAt(i) * i ) %
-            this.data.length
-            // console.log(hash)
-        }
-        return hash
-    }
+//     _hash(key) {
+//         let hash = 0;
+//         for (let i= 0; i < key.length; i++) {
+//             hash = (hash + key.charCodeAt(i) * i ) %
+//             this.data.length
+//             // console.log(hash)
+//         }
+//         return hash
+//     }
 
-    set(key, value) {
-        let address = this._hash(key);
-        if (!this.data[address]) {
-            this.data[address] =[];
-        }
-        this.data[address].push([key, value])
-        return this.data
-    }
-    get(key) {
-        let address = this._hash(key);
-        const currentBucket = this.data[address];
-        if(currentBucket) {
-            for (let i = 0; i<currentBucket.length; i++){
-                if(currentBucket[i][0] === key ) {
-                    return currentBucket[i][1]
-                }
-            }
-        }
-        return undefined
-    }
+//     set(key, value) {
+//         let address = this._hash(key);
+//         if (!this.data[address]) {
+//             this.data[address] =[];
+//         }
+//         this.data[address].push([key, value])
+//         return this.data
+//     }
+//     get(key) {
+//         let address = this._hash(key);
+//         const currentBucket = this.data[address];
+//         if(currentBucket) {
+//             for (let i = 0; i<currentBucket.length; i++){
+//                 if(currentBucket[i][0] === key ) {
+//                     return currentBucket[i][1]
+//                 }
+//             }
+//         }
+//         return undefined
+//     }
 
-    keys() {    
-        const keysArray = []; 
-            for (let i=0; i < this.data.length ; i++) {
-                if(this.data[i]) {
-                    keysArray.push(this.data[i][0][0]) 
-                    console.log(this.data[i][0][0])
+//     keys() {    
+//         const keysArray = []; 
+//             for (let i=0; i < this.data.length ; i++) {
+//                 if(this.data[i]) {
+//                     keysArray.push(this.data[i][0][0]) 
+//                     console.log(this.data[i][0][0])
                                      
-                }                 
-            }
-        return keysArray;
-    }
-}
+//                 }                 
+//             }
+//         return keysArray;
+//     }
+// }
 
-const myHashTable = new HashTable(50)
+// const myHashTable = new HashTable(50)
+// // myHashTable.set('grapes', 10000)
+// // myHashTable.get('grapes')
 // myHashTable.set('grapes', 10000)
-// myHashTable.get('grapes')
-myHashTable.set('grapes', 10000)
-myHashTable.set('apples', 54)
-myHashTable.set('oranges', 2)
-// myHashTable.keys();
+// myHashTable.set('apples', 54)
+// myHashTable.set('oranges', 2)
+// // myHashTable.keys();
 
-// console.log(myHashTable._hash('grapes'))
-// console.log(myHashTable.set('grapes', 10000), myHashTable.set('apples', 54)) 
-// console.log(myHashTable.get('oranges'));
-console.log(myHashTable.keys());
+// // console.log(myHashTable._hash('grapes'))
+// // console.log(myHashTable.set('grapes', 10000), myHashTable.set('apples', 54)) 
+// // console.log(myHashTable.get('oranges'));
+// console.log(myHashTable.keys());
+
+function findingRecurringCharacter(input) {
+    for (let i=0; i < input.length; i++) {
+        for(let j=i+1; j < input.length; j++) {
+            if(input[i] === input[j]) {
+                return input[i]
+            }
+        }
+    }
+    return undefined
+}
+//O(n^2)
+
+function findingRecurringCharacter1(input) {
+    let map = {};
+    for(let i=0; i<input.length; i++){
+        // console.log(map[input[i]])
+        if (map[input[i]] !== undefined) {
+            return input[i]
+        } else {
+            map[input[i]] = i
+        }
+        console.log(map)
+    }
+    
+    return undefined
+}
+// findingRecurringCharacter1([2,5,1,2,3,5,1,2,4])
+console.log(findingRecurringCharacter([2,5,5,2,3,5,1,2,4]))
