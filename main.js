f3f4839042f29590892052eed8b0ed9c4e53b75c
Mclakes <lekanhayo@gmail.com>
2022-11-18 01:53:07 +0100

    first linked list

--- main.js
+++ main.js
@@ -144,10 +144,39 @@
 // // findingRecurringCharacter1([2,5,1,2,3,5,1,2,4])
 // console.log(findingRecurringCharacter([2,5,5,2,3,5,1,2,4]))
 
-let obj1 = {a:true};
-let obj2 = obj1;
-obj1.a = 'booya';
-delete obj1;
-obj2 = 'hello'
-// console.log('1', obj1)
-console.log('2', obj2);
+// let obj1 = {a:true};
+// let obj2 = obj1;
+// obj1.a = 'booya';
+// delete obj1;
+// obj2 = 'hello'
+// // console.log('1', obj1)
+// console.log('2', obj2);
+
+//10-->5-->16
+
+// let myLinkedList = {
+//     head: {
+//         value: 10,
+//         next: {
+//             value: 5,
+//             next: {
+//                 value: 16,
+//                 next: null
+//             }
+//         }
+//     }
+// }
+
+class LinkedList {
+    constructor(value){
+        this.head = {
+            value: 10,
+            next: null
+        }
+            this.tail = this.head;
+            this.length = 1;
+    }
+}
+
+const myLinkedList = new LinkedList(10)
+console.log(myLinkedList)
