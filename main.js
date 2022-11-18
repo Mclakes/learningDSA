051919519c1279fa2933ee0fb772edcf2d20e5f8
Mclakes <lekanhayo@gmail.com>
2022-11-18 01:37:12 +0100

    pointers for linke list

--- main.js
+++ main.js
@@ -115,31 +115,39 @@
 // // console.log(myHashTable.get('oranges'));
 // console.log(myHashTable.keys());
 
-function findingRecurringCharacter(input) {
-    for (let i=0; i < input.length; i++) {
-        for(let j=i+1; j < input.length; j++) {
-            if(input[i] === input[j]) {
-                return input[i]
-            }
-        }
-    }
-    return undefined
-}
-//O(n^2)
+// function findingRecurringCharacter(input) {
+//     for (let i=0; i < input.length; i++) {
+//         for(let j=i+1; j < input.length; j++) {
+//             if(input[i] === input[j]) {
+//                 return input[i]
+//             }
+//         }
+//     }
+//     return undefined
+// }
+// //O(n^2)
 
-function findingRecurringCharacter1(input) {
-    let map = {};
-    for(let i=0; i<input.length; i++){
-        // console.log(map[input[i]])
-        if (map[input[i]] !== undefined) {
-            return input[i]
-        } else {
-            map[input[i]] = i
-        }
-        console.log(map)
-    }
+// function findingRecurringCharacter1(input) {
+//     let map = {};
+//     for(let i=0; i<input.length; i++){
+//         // console.log(map[input[i]])
+//         if (map[input[i]] !== undefined) {
+//             return input[i]
+//         } else {
+//             map[input[i]] = i
+//         }
+//         console.log(map)
+//     }
     
-    return undefined
-}
-// findingRecurringCharacter1([2,5,1,2,3,5,1,2,4])
-console.log(findingRecurringCharacter([2,5,5,2,3,5,1,2,4]))
+//     return undefined
+// }
+// // findingRecurringCharacter1([2,5,1,2,3,5,1,2,4])
+// console.log(findingRecurringCharacter([2,5,5,2,3,5,1,2,4]))
+
+let obj1 = {a:true};
+let obj2 = obj1;
+obj1.a = 'booya';
+delete obj1;
+obj2 = 'hello'
+// console.log('1', obj1)
+console.log('2', obj2);
