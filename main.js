44c27d2986555a91c66c1e66ef97f228f2281555
Mclakes <lekanhayo@gmail.com>
2022-11-19 21:21:40 +0100

    adding printList problem

--- main.js
+++ main.js
@@ -167,6 +167,37 @@
 //     }
 // }
 
+
+// class Node {
+//     constructor(value) {
+//         this.value = value
+//         this.next = null;
+//     }
+// }
+// class LinkedList {
+//     constructor(value){
+//         this.head = {
+//             value: 10,
+//             next: null
+//         }
+//         this.tail = this.head;
+//         this.length = 1;
+//     }
+//     append(value) {
+//         const newNode = new Node(value)
+//         this.tail.next = newNode;
+//         this.tail =newNode
+//         this.length++;
+//         return this;
+//     }
+//     prepend(value) {
+//         const newNode = new Node(value)
+//         newNode.next = this.head
+//         this.head = newNode;
+//         this.lenght++;
+//         return this;
+//     }
+// }
 class LinkedList {
     constructor(value){
         this.head = {
@@ -196,10 +227,28 @@ class LinkedList {
         this.lenght++;
         return this;
     }
+    printList() {
+        const array =[];
+        let currentNode = this.node;
+        while (currentNode !== null) {
+            array.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+        if (index === 0) {
+            this.prepend(value);
+            return this.printList();
+          }
+        return array;
+    }
+
+    insert(index, value){
+
+    }
 }
 
 const myLinkedList = new LinkedList(10)
 myLinkedList.append(5);
 myLinkedList.append(16);
-myLinkedList.prepend(1)
-console.log(myLinkedList)
+myLinkedList.prepend(1);
+// myLinkedList.insert(2, 99)
+console.log(myLinkedList.printList())
