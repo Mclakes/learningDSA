a271edfedee4948e50933f2671ecbcf96896846e
Mclakes <lekanhayo@gmail.com>
2022-11-24 21:41:32 +0100

    stack implemented

--- stacks.js
+++ stacks.js
@@ -1,3 +1,10 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
 class Stack {
     constructor(value) {
         this.top = null;
@@ -9,13 +16,40 @@ class Stack {
         return this.top;
     }
     push (value) {
-
+        const newNode = new Node(value);
+        if (this.length === 0 ){
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            const holdingPointer = this.top;
+            this.top = newNode;
+            this.top.next = holdingPointer; 
+        }
+        this.length++;
+        return this;
     }
-    pop (value) {
-
+    pop () {
+        if (!this.top) {
+            return null;
+        }
+        if (this.top === this.bottom) {
+            this.bottom = null
+        }
+        const holdingPointer = this.top;
+        this.top = this.top.next;
+        this.length--;
+        // return holdingPointer;
+        return this;
     }
     // isEmpty
 }
 
 const myStack = new Stack();
-console.log(myStack.peek())
+myStack.push("google");
+myStack.push("udemy");
+myStack.push("discord");
+myStack.peek();
+myStack.pop();
+myStack.pop();
+console.log(myStack.pop());
+
