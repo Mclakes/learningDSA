820975af85c7d8e32adb753cc2e8cc40e2f11cbf
Mclakes <lekanhayo@gmail.com>
2022-11-25 23:37:49 +0100

    bts

--- trees.js
+++ trees.js
@@ -1,5 +1,62 @@
-function BinaryTreeNode (values) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
+// function BinaryTreeNode (values) {
+//     this.value = value;
+//     this.left = null;
+//     this.right = null;
+// }
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+    insert(value) {
+        const newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode; 
+        } else {
+            let currentNode = this.root;
+            while(true) {
+                if (value < currentNode.value) {
+                    // left
+                    if(!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this
+                    }
+                    currentNode = currentNode.left
+                } else {
+                    //right
+                    if(!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.right
+                }
+            }
+        }
+    }
+
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+console.log(tree.insert(1));
+console.log( JSON.stringify(traverse(tree.root)))
+
+function traverse(node) {
+    const tree = {value: node.value };
+    tree.left = node.left === null ? null : traverse(node.left);
+    tree.right = node.right === null ? null : traverse(node.right);
+    return tree;
 }
