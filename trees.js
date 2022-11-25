fcf3ea3c09eb29e2f36ecfff9558cacf878ed8ec
Mclakes <lekanhayo@gmail.com>
2022-11-26 00:19:07 +0100

    working on remove for tree

--- trees.js
+++ trees.js
@@ -57,6 +57,34 @@ class BinarySearchTree {
       }
       return false;
     }
+    remove() {
+        if(!this.root) {
+            return false;
+        }
+        let currentNode = this.root;
+        let parentNode = null;
+        while(currentNode) {
+            if (value < currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.left; 
+            } else if (value > currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.right; 
+            }  else if (currentNode.value === value) {
+                if (currentNode.right === null) {
+                    if(parentNode === null) {
+                        this.root = currentNode.left;
+                    } else {
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.left;
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.left
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 }
 
